f926227d09dd36942ac7bbcaf05986f97e5dcc17
Henry Ozoebuka <hintlord@gmail.com>
2023-10-24 11:31:10 +0100

    add responsive mobile view menu.

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const date = new Date();
 var year = date.getFullYear();
 $("#year").text(year);
 
-//$(".nav-button").click(function(){
-//    $("nav").css("display", "block")
-    
-    
-//});
+$(".nav-button").click(function(){
+    $(".nav-ul-m").slideToggle();
+});
